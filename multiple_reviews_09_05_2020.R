53b3bbd3dd0adcca6b682f0840acf35a2b3161fb
joaoprvictor <73968857+joaoprvictor@users.noreply.github.com>
2021-05-31 11:11:32 -0300

    Update multiple_reviews_09_05_2020.R

--- multiple_reviews_09_05_2020.R
+++ multiple_reviews_09_05_2020.R
@@ -23,7 +23,7 @@ reviews <- data.frame()
 # base url has everything by the last number, which indicates page number
 product_1 <- "https://www.amazon.com/Apple-iPhone-XR-Fully-Unlocked/product-reviews/B07P6Y8L3F/ref=cm_cr_dp_d_show_all_btm?ie=UTF8&reviewerType=all_reviews&pageNumber="
 product_2 <- "https://www.amazon.com/Samsung-Unlocked-Smartphone-Pro-Grade-SM-G998UZSAXAA/product-reviews/B08N3BYNDN/ref=cm_cr_arp_d_paging_btm_next_2?ie=UTF8&reviewerType=all_reviews&pageNumber="
-product_3 <- "https://www.amazon.com/Rechargeable-Personal-Wand-Massager-Patterns/product-reviews/B08HD8MPGX/ref=cm_cr_arp_d_paging_btm_next_2?ie=UTF8&reviewerType=all_reviews&pageNumber="
+product_3 <- "https://www.amazon.com/All-New-Toshiba-43LF421U21-43-inch-Smart/product-reviews/B086VR9J8Q/ref=cm_cr_dp_d_show_all_btm?ie=UTF8&reviewerType=all_reviews&pageNumber="
 urls <- list(product_1,product_2, product_3)
 
 for (j in urls){
